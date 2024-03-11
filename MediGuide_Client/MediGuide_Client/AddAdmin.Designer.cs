fbdb4d0104d9a4ca776c8c6a578e1c5977c04df3
Parzival-007 <samrat1544@gmail.com>
2024-03-11 19:12:52 +0530

    forms

---
+++ MediGuide_Client/MediGuide_Client/AddAdmin.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace MediGuide_Client
+{
+    partial class AddAdmin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.password = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.lname = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.fname = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.adminID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.Info;
+            this.label2.Font = new System.Drawing.Font("Austral Sans Stamp Reg", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(339, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 74);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "MediGuide";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(334, 131);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(216, 36);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Add a new admin:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(514, 410);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(140, 22);
+            this.textBox5.TabIndex = 54;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(335, 403);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(130, 36);
+            this.label7.TabIndex = 53;
+            this.label7.Text = "PhoneNo. :";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(514, 373);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(140, 22);
+            this.textBox6.TabIndex = 52;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(335, 366);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(64, 36);
+            this.label8.TabIndex = 51;
+            this.label8.Text = "DoB:";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(514, 334);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(140, 22);
+            this.textBox7.TabIndex = 50;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(334, 327);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(112, 36);
+            this.label9.TabIndex = 49;
+            this.label9.Text = "Address:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(514, 297);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(140, 22);
+            this.textBox4.TabIndex = 48;
+            this.textBox4.UseSystemPasswordChar = true;
+            this.textBox4.Visible = false;
+            // 
+            // password
+            // 
+            this.password.AutoSize = true;
+            this.password.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.password.Location = new System.Drawing.Point(335, 290);
+            this.password.Name = "password";
+            this.password.Size = new System.Drawing.Size(129, 36);
+            this.password.TabIndex = 47;
+            this.password.Text = "Password:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(514, 260);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(140, 22);
+            this.textBox3.TabIndex = 46;
+            // 
+            // lname
+            // 
+            this.lname.AutoSize = true;
+            this.lname.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lname.Location = new System.Drawing.Point(335, 253);
+            this.lname.Name = "lname";
+            this.lname.Size = new System.Drawing.Size(137, 36);
+            this.lname.TabIndex = 45;
+            this.lname.Text = "Last Name:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(514, 226);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 22);
+            this.textBox2.TabIndex = 44;
+            // 
+            // fname
+            // 
+            this.fname.AutoSize = true;
+            this.fname.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.fname.Location = new System.Drawing.Point(334, 219);
+            this.fname.Name = "fname";
+            this.fname.Size = new System.Drawing.Size(138, 36);
+            this.fname.TabIndex = 43;
+            this.fname.Text = "First Name:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(514, 191);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(140, 22);
+            this.textBox1.TabIndex = 42;
+            // 
+            // adminID
+            // 
+            this.adminID.AutoSize = true;
+            this.adminID.Font = new System.Drawing.Font("Poppins Medium", 12F, System.Drawing.FontStyle.Bold);
+            this.adminID.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.adminID.Location = new System.Drawing.Point(334, 184);
+            this.adminID.Name = "adminID";
+            this.adminID.Size = new System.Drawing.Size(126, 36);
+            this.adminID.TabIndex = 41;
+            this.adminID.Text = "Admin ID :";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.Font = new System.Drawing.Font("Poppins Medium", 14F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(434, 456);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 43);
+            this.button1.TabIndex = 55;
+            this.button1.Text = "ADD";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // AddAdmin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(992, 523);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.password);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.lname);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.fname);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.adminID);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Name = "AddAdmin";
+            this.Text = "AddAdmin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label password;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label lname;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label fname;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label adminID;
+        private System.Windows.Forms.Button button1;
+    }
+}
